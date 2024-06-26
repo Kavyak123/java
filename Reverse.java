34f3c39e4d195862154cffec3f8adab8ae892fda
Kavyak123 <kuppastkavya.xworkz@gmail.com>
2024-06-26 22:13:24 +0530

    Reversing of an Array

---
+++ Reverse.java
@@ -0,0 +1,9 @@
+public class Reverse{
+public static void main(String[] args){
+int numbers[]={2,4,7,8,10,11};
+for(int i= numbers.length-1;i>=0; i--)
+{
+System.out.println("the array elements are "+numbers[i]);
+}
+}
+}
